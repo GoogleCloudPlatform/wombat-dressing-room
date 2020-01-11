cafcf78675ec550f2bb7887c21bd80a8cc96cea3
Benjamin E. Coe <bencoe@google.com>
2020-01-11 13:51:54 -0800

    Revert "fix: upgrade to @otplib/core 12 (#37)"
    This reverts commit ff7484abf15aa03eba5087ec7708fbc7182af4aa.

--- src/lib/totp-code.ts
+++ src/lib/totp-code.ts
@@ -15,11 +15,11 @@
  */
 
 import * as crypto from 'crypto';
-import {Authenticator} from '@otplib/core';
+import * as otplib from 'otplib';
 
 export const totpCode = (secret: string, epoch?: number) => {
   if (epoch) epoch = epoch / 1000;
-  const authen = new Authenticator();
+  const authen = new otplib.authenticator.Authenticator();
   authen.options = {epoch, crypto};
   return authen.generate(secret);
 };
