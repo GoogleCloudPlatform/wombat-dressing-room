fd56ca13e2a6ec4c7b07d13cffd2fcbf58c2a3e9
Benjamin E. Coe <bencoe@google.com>
2020-01-11 12:22:25 -0800

    test: building framework for testing (#34)

--- test/lib/write-package.ts
+++ test/lib/write-package.ts
@@ -2,11 +2,12 @@ import {expect} from 'chai';
 import {Request, Response} from 'express';
 import * as nock from 'nock';
 
+import {createPackument} from '../helpers/create-packument';
+import {writePackageRequest} from '../helpers/write-package-request';
+
 import * as datastore from '../../src/lib/datastore';
 import {PublishKey, User} from '../../src/lib/datastore';
 import {writePackage, WriteResponse} from '../../src/lib/write-package';
-
-import request = require('request');
 
 nock.disableNetConnect();
 
@@ -26,6 +27,42 @@ describe('writePackage', () => {
     const ret = await writePackage('@soldair/foo', req, res);
     expect(ret.statusCode).to.equal(401);
     expect(ret.error).to.match(/publish key not found/);
+  });
+
+  it('responds with 400 if packument has no repository field', async () => {
+    // Fake that there's a releaseAs2FA key in datastore:
+    writePackage.datastore = Object.assign({}, datastore, {
+      getPublishKey: async (username: string): Promise<PublishKey | false> => {
+        return {
+          username: 'bcoe',
+          created: 1578630249529,
+          value: 'deadbeef',
+          releaseAs2FA: true,
+        };
+      },
+      getUser: async (username: string): Promise<false | User> => {
+        return {name: 'bcoe', token: 'deadbeef'};
+      },
+    });
+
+    // Simulate a publication request to the proxy:
+    const req = writePackageRequest(
+      {authorization: 'token: abc123'},
+      createPackument('@soldair/foo')
+        .addVersion('1.0.0')
+        .packument()
+    );
+    const res = {status: (code: number) => {}, end: () => {}} as Response;
+
+    // A 404 while fetching the packument indicates that the package
+    // has not yet been created:
+    const npmRequest = nock('https://registry.npmjs.org')
+      .get('/@soldair%2ffoo')
+      .reply(404);
+
+    const ret = await writePackage('@soldair/foo', req, res);
+    expect(ret.error).to.match(/must have a repository/);
+    expect(ret.statusCode).to.equal(400);
   });
 
   describe('releaseAs2FA', () => {
@@ -55,34 +92,13 @@ describe('writePackage', () => {
         return Promise.resolve({statusCode: 200, newPackage});
       };
 
-      // Simulate a package publication request:
-      const req = {
-        headers: {authorization: 'token: abc123'},
-        on: (event: 'data' | 'end', listener: (buffer?: Buffer) => void) => {
-          switch (event) {
-            case 'data':
-              listener(
-                Buffer.from(
-                  JSON.stringify({
-                    versions: {
-                      '1.0.0': {
-                        version: '1.0.0',
-                        repository: 'https://github.com/foo/bar',
-                      },
-                    },
-                    'dist-tags': {latest: '1.0.0'},
-                  })
-                )
-              );
-              break;
-            case 'end':
-              listener();
-              break;
-            default:
-              break;
-          }
-        },
-      } as Request;
+      // Simulate a publication request to the proxy:
+      const req = writePackageRequest(
+        {authorization: 'token: abc123'},
+        createPackument('@soldair/foo')
+          .addVersion('1.0.0', 'https://github.com/foo/bar')
+          .packument()
+      );
       const res = {status: (code: number) => {}, end: () => {}} as Response;
 
       // A 404 while fetching the packument indicates that the package
